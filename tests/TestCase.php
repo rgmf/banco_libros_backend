fe7489fcbfe10352902f6e08a7ef50f0b2822860
Román Martínez <rgmf@riseup.net>
2023-03-23 15:04:02 +0100

    Created a testing database in a separate Docker container.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,14 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    // It is often useful to reset your database after each test so that data
+    // from a previous test does not interfere with subsequent tests.
+    use RefreshDatabase;
 }
