45b181c86eefe8d776188f23d80c8b02b42c34d4
Román Martínez <rgmf@riseup.net>
2023-06-26 15:51:29 +0200

    Added more grades into migration.

--- database/migrations/2023_03_20_173027_add_data_to_grades.php
+++ database/migrations/2023_03_20_173027_add_data_to_grades.php
@@ -15,11 +15,23 @@ return new class extends Migration
         DB::table('grades')->insert(['id' => 3, 'name' => '3º ESO']);
         DB::table('grades')->insert(['id' => 4, 'name' => '4º ESO']);
 
-        DB::table('grades')->insert(['id' => 5, 'name' => '1º FPB']);
-        DB::table('grades')->insert(['id' => 6, 'name' => '2º FPB']);
+        DB::table('grades')->insert(['id' => 20, 'name' => '1º FPB Peluquería']);
+        DB::table('grades')->insert(['id' => 21, 'name' => '2º FPB Peluquería']);
+        DB::table('grades')->insert(['id' => 22, 'name' => '1º FPB Estética']);
+        DB::table('grades')->insert(['id' => 23, 'name' => '2º FPB Estética']);
+        DB::table('grades')->insert(['id' => 24, 'name' => '1º FPB Informática']);
+        DB::table('grades')->insert(['id' => 25, 'name' => '2º FPB Informática']);
+        DB::table('grades')->insert(['id' => 26, 'name' => '1º FPB Electricidad']);
+        DB::table('grades')->insert(['id' => 27, 'name' => '2º FPB Electricidad']);
 
-        DB::table('grades')->insert(['id' => 7, 'name' => '1º Bachillerato']);
-        DB::table('grades')->insert(['id' => 8, 'name' => '2º Bachillerato']);
+        DB::table('grades')->insert(['id' => 30, 'name' => '1º Bach Ciencias']);
+        DB::table('grades')->insert(['id' => 31, 'name' => '2º Bach Ciencias']);
+        DB::table('grades')->insert(['id' => 32, 'name' => '1º Bach Humanidades']);
+        DB::table('grades')->insert(['id' => 33, 'name' => '2º Bach Humanidades']);
+        DB::table('grades')->insert(['id' => 34, 'name' => '1º Bach CCSS']);
+        DB::table('grades')->insert(['id' => 35, 'name' => '2º Bach CCSS']);
+        DB::table('grades')->insert(['id' => 36, 'name' => '1º Bach General']);
+        DB::table('grades')->insert(['id' => 37, 'name' => '2º Bach General']);
     }
 
     /**
